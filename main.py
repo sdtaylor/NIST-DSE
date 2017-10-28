d9eb69949b0ad047b7c207c83c4e3cac5dc99657
Shawn Taylor <shawntaylor@weecology.org>
2017-10-28 18:47:23 -0400

    next step in primary script

--- main.py
+++ main.py
@@ -11,3 +11,6 @@ training_plots = load_plots(plot_list = train_plots[1:3], plot_type= 'train')
 for p in training_plots:
     ndvi_data = ndvi_from_hs(p.images['hs'].image_data)
     p.load_image(image_type='ndvi', image_data=ndvi_data)
+
+model = watershed_classifier()
+model.fit(training_plots)
